e6399d27c0bd1eeb32c0001d2d883a3bd66be0fb
Jiéverson Maissiat <jieverson@gmail.com>
2018-06-27 02:50:34 +0000

    play chord progression

--- bundle.js
+++ bundle.js
@@ -2,28 +2,50 @@
 const teoria = require('teoria')
 const Tone = require('tone')
 
-const synth = new Tone.PolySynth(8, Tone.Synth)
+synth = new Tone.PolySynth(8, Tone.Synth)
         .toMaster()
 
 const play = () => {
-    let chordStr = document
+    stop()
+
+    let input = document
         .getElementById('chord').value
-
-    let chord = teoria.chord(chordStr)
-    console.log(chord.toString())
     
-    let notes = chord.notes()
-        .map(x => x.name() 
-            + x.accidental() 
-            + x.octave())
-    
-    console.log(notes)
+    let chords = 
+        input.split('|')
+        .map(x => x.trim())
+        .map(x => teoria.chord(x))
+        .map(x => x.notes()
+            .map(n => n.name().toUpperCase()
+            + n.accidental() 
+            + n.octave()))
+        .reduce((a, x, i) => {
+            a.push([i, x]); 
+            return a 
+        }, [])
+   
+    let progression = new Tone.Part((time, chord) => {
+        document.getElementById('current').innerText = chord
+        synth.triggerAttackRelease(chord, "2n", time)
+    }, chords).start(0)
 
-    synth.triggerAttackRelease(notes, "2n")
+    progression.loop = true;
+	progression.loopEnd = chords.length;
+
+    Tone.Transport.bpm.value = 140;   
+    Tone.Transport.start("+0.1");
+}
+
+const stop = () => {
+    Tone.Transport.cancel();
+    document.getElementById('current').innerText = null
 }
 
 document.getElementById('player')
     .onclick = play
+
+document.getElementById('stop')
+    .onclick = stop
 },{"teoria":9,"tone":17}],2:[function(require,module,exports){
 var accidentalValues = {
   'bb': -2,
